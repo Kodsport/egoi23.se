279c8d51d023aa575d7fcc24f5e574a9f098d81f
Erik Amirell Eklöf <erik.amirell.eklof@gmail.com>
2023-07-10 12:52:04 +0200

    updated delegations list

--- process_delegations_csv.py
+++ process_delegations_csv.py
@@ -14,22 +14,40 @@ ROLES_TO_INCLUDE = ["Contestant", "Leader", "Deputy Leader"]
 
 skipped_roles = set()
 
+pictures_dir_path = sys.argv[2]
+
 delegations = {}
 
 with open(sys.argv[1], newline='') as fp:
 	csv_reader = csv.reader(fp)
-	for row in csv_reader:
+	rows = list(csv_reader)
+	first_row = rows.pop(0)
+	consent_form_col_idx = first_row.index("consent form filename")
+	picture_col_idx = first_row.index("profile picture filename")
+	for row in rows:
 		if len(row) < 4:
 			continue
 		country, role, passport_name, display_name = row[:4]
 		if not display_name:
 			continue
-		if role in ROLES_TO_INCLUDE:
-			if country not in delegations:
-				delegations[country] = []
-			delegations[country].append((role, display_name, passport_name))
-		else:
+		if role not in ROLES_TO_INCLUDE:
 			skipped_roles.add(role)
+			continue
+		
+		consent_form_link = row[consent_form_col_idx].lower()
+		if consent_form_link:
+			if "consent" not in consent_form_link:
+				print(f"warning! filename does not look like a consent form: {consent_form_link}")
+				consent_form_link = ""
+			elif not os.path.exists(pictures_dir_path + "/" + consent_form_link):
+				print(f"warning! consent form file does not exist: " + pictures_dir_path + "/" + consent_form_link)
+				consent_form_link = ""
+		else:
+			print(f"no consent form for {display_name}")
+		if country not in delegations:
+			delegations[country] = []
+		delegations[country].append((role, display_name, passport_name, row[picture_col_idx].lower(), bool(consent_form_link)))
+
 print("skipped roles:", skipped_roles)
 
 delegations = list(delegations.items())
@@ -43,75 +61,16 @@ def remove_repeated_underscore(s: str):
 		result.append(c)
 	return "".join(result)
 
-consent_forms = set()
-profile_picture_paths = {}
-pictures_dir_path = sys.argv[2]
-for file_name in os.listdir(pictures_dir_path):
-	file_name_lower = file_name.lower()
-	
-	file_name_lower = file_name_lower.replace("photo", "picture")
-	file_name_lower = file_name_lower.replace("pricture", "picture")
-	file_name_lower = file_name_lower.replace("profie", "profile")
-	file_name_lower = file_name_lower.replace("pofile", "profile")
-	file_name_lower = file_name_lower.replace("plofile", "profile")
-	
-	file_name_lower = remove_repeated_underscore(file_name_lower.replace(" ", "_"))
-	
-	if "picture" in file_name_lower:
-		name = file_name_lower[:file_name_lower.index("picture")]
-		name = name.removesuffix("profile_")
-		name = name.removesuffix("_")
-		name = unidecode(name)
-		profile_picture_paths[name] = file_name
-	elif "consent_form" in file_name_lower:
-		name = file_name_lower[:file_name_lower.index("consent_form")]
-		name = name.removesuffix("_")
-		name = unidecode(name)
-		consent_forms.add(name)
-	else:
-		print("unrecognized file name format: " + file_name)
-
-def unidecode2(s: str):
-	return s.replace("ö", "oe").replace("ø", "oe").replace("å", "aa")
-
-def find_profile_picture(name):
-	name = unidecode(remove_repeated_underscore(name.lower().replace(" ", "_").replace(",", "")))
-	if name in profile_picture_paths and name in consent_forms:
-		return profile_picture_paths[name]
-	
-	name_parts = name.split("_")
-	name_without_underscore = name.replace("_", "")
-	matches = []
-	for picture_name, file_name in profile_picture_paths.items():
-		if picture_name not in consent_forms:
-			continue
-		if picture_name.replace("_", "") == name_without_underscore:
-			matches.append(file_name)
-			continue
-		picture_name_parts = picture_name.split("_")
-		if name_parts[0] in [picture_name_parts[0], picture_name_parts[-1]] and all(map(lambda p: p in name_parts, picture_name_parts)):
-			matches.append(file_name)
-	if len(matches) == 1:
-		return matches[0]
-	elif len(matches) > 1:
-		print(f"multiple matches for {name}:", matches)
-	return None
-
-missing_profile_pictures = 0
-
 delegations_yaml_lines = []
 images_to_copy = []
 
 for country_name, people in delegations:
-	print(country_name)
+	#print(country_name)
 	delegations_yaml_lines.append(f"- name: {country_name}")
 	delegations_yaml_lines.append("  people:")
 	people.sort(key=lambda p: (ROLES_TO_INCLUDE.index(p[0]), p[1]))
-	for idx, (role, display_name, passport_name) in enumerate(people):
-		profile_picture_path = find_profile_picture(display_name) or find_profile_picture(passport_name) or find_profile_picture(unidecode2(display_name))
-		if profile_picture_path is None:
-			missing_profile_pictures += 1
-		print(f"  {role:<15}  {display_name:>35}  {profile_picture_path}")
+	for idx, (role, display_name, passport_name, profile_picture_path, has_consent_form) in enumerate(people):
+		#print(f"  {role:<15}  {display_name:>35}  {profile_picture_path}")
 		
 		delegations_yaml_lines.append(f"  - name: {display_name}")
 		if role == "Leader":
@@ -122,12 +81,14 @@ for country_name, people in delegations:
 			img_suffix = "_dtl"
 		else:
 			img_suffix = "_c" + str(idx + 1)
-		if profile_picture_path:
-			img_name = unidecode(country_name).lower().replace(' ', '_') + img_suffix + ".jpg"
-			delegations_yaml_lines.append("    img: " + img_name)
-			images_to_copy.append((pictures_dir_path + "/" + profile_picture_path, img_name))
-
-print("missing pictures:", missing_profile_pictures)
+		if profile_picture_path and has_consent_form:
+			full_path = pictures_dir_path + "/" + profile_picture_path
+			if os.path.exists(full_path):
+				img_name = unidecode(country_name).lower().replace(' ', '_') + img_suffix + ".jpg"
+				delegations_yaml_lines.append("    img: " + img_name)
+				images_to_copy.append((pictures_dir_path + "/" + profile_picture_path, img_name))
+			else:
+				print("profile picture not found: " + full_path)
 
 with open("_data/delegations.yml", "w") as delegations_yml_fp:
 	delegations_yml_fp.write("\n".join(delegations_yaml_lines))
